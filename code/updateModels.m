fa17da0d0af1a7211e0f0bb64b60c40c8d6cab1c
cellid-hub <78161856+cellid-hub@users.noreply.github.com>
2022-11-18 13:34:48 -0500

    added update to updatemodels

--- code/updateModels.m
+++ code/updateModels.m
@@ -19,7 +19,7 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
     if(fc > fcutoff)
        sigma = SigmaMin + A*((fc - fcutoff)^R);    
     end
-    d = bwdist(ProbMaskThreshold);
+    
     LocalWindows = getLocalWindows();
 
     ColorModels = getShapeModels(maskSize, LocalWindows, ColorConfidences,...
@@ -27,19 +27,102 @@ function [mask, LocalWindows, ColorModels, ShapeConfidences] = ...
    
     mask = getMask(ColorModels,ShapeConfidences);
     
-end
-
-function models = getShapeModels(maskSize, LocalWindows, ColorConfidences, window_d_matrices, WindowWidth, SigmaMin, A, fcutoff, R)
-    models = initShapeConfidences(maskSize, LocalWindows, ColorConfidences, window_d_matrices, WindowWidth, SigmaMin, A, fcutoff, R);
-end
-
-function models = getColorModels(ColorModels,warpedMask,warpedMaskOutline)
-    FG_Theshold = .75;
-    BG_Theshold = .25;
-    %call the initializeColorModels method? fitgmdist
     
 end
 
+function models = getShapeConfidences()
+    %updating the shape confidences by recalculating it with the new color
+    %confidences and windows
+    models = initShapeConfidences(maskSize, LocalWindows, ColorConfidences, window_d_matrices, WindowWidth, SigmaMin, A, fcutoff, R);
+end
+
+function models = getShapeModels(maskSize, LocalWindows, ColorConfidences, window_d_matrices, WindowWidth, SigmaMin, A, fcutoff, R)
+    %Encorporates the foreground mask and the new shape confidence 
+end
+
+function ColorModels = getColorModels(CurrentFrame, warpedMask, warpedMaskOutline,...
+    NewLocalWindows, BoundaryWidth, WindowWidth, ColorModels)
+    %need figure out how to apply old GMMS to new windows
+    num_windows = size(NewLocalWindows);
+    newColorModels = initializeColorModels(CurrentFrame, warpedMask, warpedMaskOutline, NewLocalWindows, BoundaryWidth, WindowWidth);
+
+    %getting the masks for the windows
+    oldMasks = getfield( ColorModels , "window_masks" );
+    newMasks = getfield( newColorModels , "window_masks" );
+
+    %getting the foreground GMMS
+    oldFGMMS = getfield( ColorModels , "window_F_gmms" );
+    newFGMMS = getfield( newColorModels , "window_F_gmms" );
+
+    %getting the Background GMMS
+    oldBGMMS = getfield( ColorModels , "window_B_gmms" );
+    newBGMMS = getfield( newColorModels , "window_B_gmms" );
+
+    updatedFGMMs = cell(1, num_windows);
+    updatedBGMMs = cell(1, num_windows);
+    updatedColorConf = cell(1, num_windows);
+    %getting the forground prob matricies for old gmms on new windows
+    p_c_matrices = applyColorModels(oldFGMMS,oldBGMMS, num_windows,...
+        WindowWidth,Img,sigma_c,NewLocalWindows);
+    for i = [1:num_windows(1)]
+        oldArea = sum(oldMasks{i},"all");
+        newArea = sum(newMasks{i},"all");
+        if newArea > oldArea
+            %newColorModels = setfield(ColorModels,"f_c_values",0);
+            updatedFGMMs{i} = oldFGMMS{i};
+            updatedBGMMs{i} = oldBGMMS{i};
+            %updatedColorConf = oldconfidence
+        else
+            updatedFGMMs{i} = newFGMMS{i};
+            updatedBGMMs{i} = newBGMMS{i};
+            %recompute color confidence values
+            %updatedColorConf = getColorConfidenceValue();
+        end
+    end
+    %figure out how to compare the areas between the two models
+    %if newcolormodel area <= old colormodel area then recompute color
+    %confidence values
+    ColorModels = struct('window_F_gmms', {updatedFGMMs},...
+        'window_B_gmms', {updatedBGMMs},...
+        'f_c_values', {updatedColorConf},...
+        'window_masks', {window_masks});
+end
+
+function masks = applyColorModels(FGMMs,BGMMs, num_windows,WindowWidth,...
+    Img,sigma_c,LocalWindows)
+
+    window_p_c_matrices = cell(1, num_windows);
+    for i=1:num_windows
+        center = LocalWindows(i,:);
+        center = [center(2) center(1)];
+        window = zeros(WindowWidth, WindowWidth, 3);
+        window = Img(startRow:endRow,startCol:endCol, :);
+        startRow = max([1, center(1) - sigma_c]);
+        endRow = (min([sz(1), center(1) + sigma_c])); 
+        for row=1:WindowWidth
+                for col=1:WindowWidth                    
+                    curr_pixel = squeeze(window(row, col, :))';                     
+                    p_F_gmm = pdf(F_gmm, curr_pixel);
+                    p_B_gmm = pdf(B_gmm, curr_pixel);
+                    window_p_c_matrix(row, col) = p_F_gmm / (p_F_gmm + p_B_gmm);
+                end
+        end
+        window_p_c_matrices{i} = window_p_c_matrix;
+    end
+    masks = window_p_c_matrices;
+end
+
+function confidences = getColorConfidenceValue(wind_num,window_p_c_matrix)    
+    window_w_c_matrix = w_c_matrix(startRow:endRow,startCol:endCol);
+    window_mask = Mask(startRow:endRow,startCol:endCol);
+    window_d_matrix = d_matrix(startRow:endRow,startCol:endCol);
+
+    up = sum(abs(window_mask - window_p_c_matrix) .* window_w_c_matrix);
+    down = sum(window_w_c_matrix);
+    f_c = 1 - up/down;    
+end
+
+%not sure how I am supposed to update the local windows
 function windows = getLocalWindows(WindowWidth,LocalWindows)
     
 end
